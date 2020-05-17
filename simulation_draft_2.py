096bedf423b3d3dcb480e4fbb7802cd105f8be8a
Sandro331 <sandroninua@gmail.com>
2020-05-17 16:00:22 -0700

    Update simulation_draft_2.py

--- simulation_draft_2.py
+++ simulation_draft_2.py
@@ -16,25 +16,22 @@ nx.draw(G)
 def BFS(Gr,zero,p): 
   
         # Mark all the vertices as not visited 
-        visited = [False] * Gr.number_of_nodes()
         infected = [False] * Gr.number_of_nodes()
         k=1
-  
         # Create a queue for BFS 
         queue = [] 
-  
+        infected_nodes = []
         # Mark the source node as  
         # visited and enqueue it 
         queue.append(zero) 
-        visited[zero] = True
         infected[zero] = True
-  
+        
         while queue: 
             
             # Dequeue a vertex from  
             # queue and print it 
             s = queue.pop(0) 
-            print (s, end = " ") 
+            infected_nodes.append(s)
   
             # Get all adjacent vertices of the 
             # dequeued vertex s. If a adjacent 
@@ -51,5 +48,42 @@ def BFS(Gr,zero,p):
           #  if infected[i]==True:
           #      print(i)
         print("inf",k)           
+        return infected_nodes
             
-BFS(G,10,0.2)
+print(BFS(G,10,0.2))
+
+def BFS_t(Gr,zero,p,d): 
+  
+        # Mark all the vertices as not visited 
+        infected = [False] * Gr.number_of_nodes()
+        symptomatic = [False] * Gr.number_of_nodes()
+        quarantined = [False] * Gr.number_of_nodes()
+        k=1
+        days_rem = d
+        # Create a queue for BFS 
+        queue = [] 
+        infected_nodes = []
+        # Mark the source node as  
+        # visited and enqueue it 
+        queue.append(zero) 
+        infected[zero] = True
+        while days_rem > 0:  
+            while queue: 
+                s = queue.pop(0) 
+                infected_nodes.append(s)
+                for i in Gr.neighbors(s): 
+                    if infected[i]==False:
+                        if rand.uniform(0,10) < p*10:
+                            infected[i] = True
+                            queue.append(i) 
+                            k+=1
+                                    
+                            
+            days_rem-=1
+            print("new day")
+            print("inf",k)
+            queue.append(rand.randint(0, len(infected_nodes)))            
+        print("inf",k)           
+        return infected_nodes
+    
+print(BFS_t(G,10,0.08,7))    
